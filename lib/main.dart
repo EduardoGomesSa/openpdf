edf4a5779d9e462d57f13589f93db5e59bde768e
Eduardo Sa <eduardo.sa.022@gmail.com>
2024-09-28 23:55:00 -0300

    Criação da home page para abstrair a logica da main

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_pdfview/flutter_pdfview.dart';
-import 'package:openpdf/src/pages/pdf_viewer_page.dart';
-import 'dart:async';
+import 'package:openpdf/src/pages/home_page.dart';
 
-import 'package:receive_sharing_intent/receive_sharing_intent.dart';
-
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  _MyAppState createState() => _MyAppState();
+  MyAppState createState() => MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  late StreamSubscription _intentSub;
-  final _sharedFiles = <SharedMediaFile>[];
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Listen to media sharing coming from outside the app while the app is in the memory.
-    _intentSub = ReceiveSharingIntent.instance.getMediaStream().listen((value) {
-      setState(() {
-        _sharedFiles.clear();
-        _sharedFiles.addAll(value);
-
-        print(_sharedFiles.map((f) => f.toMap()));
-      });
-    }, onError: (err) {
-      print("getIntentDataStream error: $err");
-    });
-
-    // Get the media sharing coming from outside the app while the app is closed.
-    ReceiveSharingIntent.instance.getInitialMedia().then((value) {
-      setState(() {
-        _sharedFiles.clear();
-        _sharedFiles.addAll(value);
-        print(_sharedFiles.map((f) => f.toMap()));
-
-        // Tell the library that we are done processing the intent.
-        ReceiveSharingIntent.instance.reset();
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    _intentSub.cancel();
-    super.dispose();
-  }
-
+class MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    const textStyleBold = const TextStyle(fontWeight: FontWeight.bold);
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: PDFView(
-        filePath: _sharedFiles.first.path,
-        enableSwipe: true,
-        swipeHorizontal: true,
-        autoSpacing: true,
-        pageFling: true,
-      ),
-      ),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
